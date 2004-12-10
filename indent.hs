260b4cb2c2bfb744afcab91dcb3ea3e162e99672
monnier <pweaver@signalicorp.com>
2004-12-10 10:23:21 -0800

    More test cases.
    Ignore-this: 6cfed81dd1d6a8d568e86b4a87aeec2
    
    darcs-hash:20041210182321-c2f2e-d3c04c2559dc9819d8267fcc690b6400f630e97f.gz

--- indent.hs
+++ indent.hs
@@ -12,7 +12,35 @@ bdigits n | n>1       = n `mod` 2 :
 bvalue :: [Int]->Int
 bvalue [] = error "bvalue of []"
 bvalue s  = bval 1 s
-            where
-            bval e []                    = 0
-            bval e (b:bs) | b==0 || b==1 = b*e + bval (2*e) bs
-                          | otherwise    = error "illegal digit"
+    where
+      bval e [] = 0           -- FIXME: indentation below `where'.
+      bval e (b:bs) | b==0 || b==1 = b*e + bval (2*e) bs
+                    | otherwise    = error "illegal digit"
+
+{- text
+   indentation
+   inside comments
+ -}
+toto a = ( hello
+         -- FIXME: indentation of this comment.
+         , there                -- FIXME: indentation of leading , and ;
+         , my friends )         -- FIXME: wrong second indentation.
+
+titi b =
+    do expr1
+       {- text
+        - indentation
+        - inside comments
+        -}
+       let foo  = bar
+           foo2 = bar2
+       expr2            -- FIXME: two possible `let's.
+
+tata c =
+    let bar = case foo
+              of 1 -> blabla
+                 2 -> blibli    -- FIXME: only one possible indentation here.
+        bar = case foo of
+                _ -> blabla    -- FIXME: indentation below `case'.
+        bar' = case foo
+           of _ -> blabla       -- FIXME: confused by the ' in bar'.
