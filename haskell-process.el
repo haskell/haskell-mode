haskell/haskell-mode

commit a7528e61040c923d289d8617d9c1548e46046761

Highlight as haskell for :type and :info.

haskell-process.el
@@ -179,7 +179,8 @@ has changed?"
      (format (if (string-match "^[_[:lower:][:upper:]]" ident)
                  ":type %s"
                ":type (%s)")
-             ident))))
+             ident))
+   'haskell-mode))
 
 ;;;###autoload
 (defun haskell-process-do-info (&optional ident)
@@ -192,7 +193,8 @@ has changed?"
                  ":info %s"
                ":info (%s)")
              (or ident
-                 (haskell-ident-at-point))))))
+                 (haskell-ident-at-point))))
+   'haskell-mode))
 
 (defun haskell-process-do-try-info (sym)
   "Get info of `sym' and echo in the minibuffer."
@@ -212,18 +214,20 @@ has changed?"
                               (string-match "^<interactive>" response))
                     (haskell-mode-message-line response)))))))
 
-(defun haskell-process-do-simple-echo (insert-value line)
+(defun haskell-process-do-simple-echo (insert-value line &optional mode)
   "Send some line to GHCi and echo the result in the REPL and minibuffer."
   (let ((process (haskell-process)))
     (haskell-process-queue-command
      process
      (make-haskell-command
-      :state (list process line insert-value)
+      :state (list process line insert-value mode)
       :go (lambda (state)
             (haskell-process-send-string (car state) (cadr state)))
       :complete (lambda (state response)
                   (haskell-interactive-mode-echo
-                   (haskell-process-session (car state)) response)
+                   (haskell-process-session (car state))
+                   response
+                   (cadddr state))
                   (haskell-mode-message-line response)
                   (when (caddr state)
                     (goto-char (line-beginning-position))

haskell-interactive-mode.el
@@ -307,17 +307,21 @@ SESSION, otherwise operate on the current buffer.
           (inhibit-read-only t))
       (delete-region start-point (point))
       (goto-char (point-max))
-      (insert (let ((mode haskell-interactive-mode-eval-mode))
-                (with-current-buffer (get-buffer-create (concat " haskell-font-lock-as-"
-                                                                (symbol-name mode)))
-                  (unless (eq major-mode mode)
-                    (funcall mode))
-                  (erase-buffer)
-                  (insert text)
-                  (font-lock-fontify-region (point-min) (point-max))
-                  (let ((result (buffer-substring (point-min) (point-max))))
-                    (erase-buffer)
-                    (concat result "\n"))))))))
+      (insert (haskell-interactive-text-as-mode (concat text "\n")
+                                                haskell-interactive-mode-eval-mode)))))
+
+(defun haskell-interactive-text-as-mode (text mode)
+  "Propertize `text' according to the font locking settings of
+`mode'."
+  (with-current-buffer (get-buffer-create (concat " haskell-font-lock-as-"
+                                                  (symbol-name mode)))
+    (unless (eq major-mode mode)
+      (funcall mode))
+    (erase-buffer)
+    (insert text)
+    (font-lock-fontify-region (point-min) (point-max))
+    (buffer-substring (point-min)
+                      (point-max))))
 
 (defun haskell-interactive-mode-eval-pretty-result (session text)
   "Insert the result of an eval as a pretty printed Showable, if
@@ -333,14 +337,18 @@ SESSION, otherwise operate on the current buffer.
       (insert "\n"))))
 
 ;;;###autoload
-(defun haskell-interactive-mode-echo (session message)
+(defun haskell-interactive-mode-echo (session message &optional mode)
   "Echo a read only piece of text before the prompt."
   (with-current-buffer (haskell-session-interactive-buffer session)
     (save-excursion
       (haskell-interactive-mode-goto-end-point)
-      (insert (propertize (concat message "\n")
-                          'read-only t
-                          'rear-nonsticky t)))))
+      (insert (if mode
+                  (haskell-interactive-text-as-mode
+                   (concat message "\n")
+                   mode)
+                (propertize (concat message "\n")
+                            'read-only t
+                            'rear-nonsticky t))))))
 
 (defun haskell-interactive-mode-compile-error (session message)
   "Echo an error."

commit 9bb08ef748ba517491814e2251aa7f70a4b8695a

Fixup forward declare-function broken by a7528e6

haskell-process.el
@@ -32,7 +32,7 @@
 (with-no-warnings (require 'cl))
 
 ;; FIXME: haskell-process shouldn't depend on haskell-interactive-mode to avoid module-dep cycles
-(declare-function haskell-interactive-mode-echo "haskell-interactive-mode" (session message))
+(declare-function haskell-interactive-mode-echo "haskell-interactive-mode" (session message &optional mode))
 (declare-function haskell-interactive-mode-compile-error "haskell-interactive-mode" (session message))
 (declare-function haskell-interactive-mode-insert "haskell-interactive-mode" (session message))
 (declare-function haskell-interactive-mode-reset-error "haskell-interactive-mode" (session))