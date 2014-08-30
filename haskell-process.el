83764cd0935e28aff22baf5b87d00f20da35216d
scturtle <scturtle@gmail.com>
2014-08-30 18:19:04 +0800

    Add support to generate tags on windows.

--- haskell-process.el
+++ haskell-process.el
@@ -263,14 +263,20 @@ imports become available?"
      (make-haskell-command
       :state (cons process and-then-find-this-tag)
       :go (lambda (state)
-            (haskell-process-send-string
-             (car state)
-             (format ":!cd %s && %s | %s | %s"
-                     (haskell-session-cabal-dir
-                      (haskell-process-session (car state)))
-                     "find . -name '*.hs*'"
-                     "grep -v '#'" ; To avoid Emacs back-up files. Yeah.
-                     "xargs hasktags -e -x")))
+            (if (eq system-type 'windows-nt)
+                (haskell-process-send-string
+                 (car state)
+                 (format ":!powershell -Command \"& { cd %s ; hasktags -e -x (ls -fi *.hs -exclude \\\"#*#\\\" -name -r) ; exit }\""
+                         (haskell-session-cabal-dir
+                          (haskell-process-session (car state)))))
+                (haskell-process-send-string
+                 (car state)
+                 (format ":!cd %s && %s | %s | %s"
+                         (haskell-session-cabal-dir
+                          (haskell-process-session (car state)))
+                         "find . -name '*.hs*'"
+                         "grep -v '#'" ; To avoid Emacs back-up files. Yeah.
+                         "xargs hasktags -e -x"))))
       :complete (lambda (state response)
                   (when (cdr state)
                     (let ((tags-file-name
