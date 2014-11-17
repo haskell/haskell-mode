4185716d3eecdca641245e838e82feaeef0dac25
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2014-11-17 07:52:11 +0100

    Refactor to use only a `haskell-process-wrapper-function`.
    Following this discussion - https://github.com/haskell/haskell-mode/pull/370#issuecomment-63237778.
    
    This permits to one's own wrapper function.
    
    For example, here is one to use with nix-shell:
    
    ```sh
    (custom-set-variables '(haskell-process-wrapper-function
                            (lambda (argv) (append (list "nix-shell" "default.nix" "--command" )
                                              (list (shell-quote-argument (mapconcat 'identity argv " ")))))))
    ```
    
    Unit tests are ok.
    Need to test in real life now.

--- tests/haskell-process-tests.el
+++ tests/haskell-process-tests.el
@@ -14,102 +14,90 @@
 
 (require 'el-mock)
 
-(ert-deftest haskell-process-stringify-cmd-no-arg ()
-  "No wrapper, return directly the command (between string quote)."
-  (should (equal "run"
-                 (let ((haskell-process-wrapper nil))
-                   (haskell-process-stringify-cmd "run")))))
-
-(ert-deftest haskell-process-stringify-cmd-with-args ()
-  "No wrapper, return directly the command."
-  (should (equal "run -a b -c d"
-                 (let ((haskell-process-wrapper nil))
-                   (haskell-process-stringify-cmd "run" '("-a" "b" "-c" "d"))))))
-
-(ert-deftest haskell-process-wrapper-command-nil ()
+(ert-deftest haskell-process-wrapper-command-function-identity ()
   "No wrapper, return directly the command."
   (should (equal '("ghci")
-                 (let ((haskell-process-wrapper nil))
-                   (haskell-process-wrapper-command "ghci")))))
+                 (progn
+                   (custom-set-variables '(haskell-process-wrapper-function #'identity))
+                   (apply haskell-process-wrapper-function (list '("ghci")))))))
 
-(ert-deftest haskell-process-wrapper-command-with-string ()
+(ert-deftest haskell-process-wrapper-function-non-identity ()
   "Wrapper as a string, return the wrapping command as a string."
-  (should (equal '("nix-shell" "cabal run")
-                 (let ((haskell-process-wrapper "nix-shell"))
-                   (haskell-process-wrapper-command "cabal run")))))
-
-(ert-deftest haskell-process-wrapper-command-with-string-2 ()
-  "Wrapper as a string, return the wrapping command as a string."
-  (should (equal '("nix-shell" "cabal repl")
-                 (let ((haskell-process-wrapper "nix-shell"))
-                   (haskell-process-wrapper-command "cabal" '("repl"))))))
-
-(ert-deftest haskell-process-wrapper-command-with-repeat-string ()
-  "Wrapper as a list of string, return the wrapping command as a string."
-  (should (equal '("nix-shell" "default.nix" "--command" "cabal build")
-                 (let ((haskell-process-wrapper '("nix-shell" "default.nix" "--command")))
-                   (haskell-process-wrapper-command "cabal" '("build"))))))
+  (should (equal '("nix-shell" "default.nix" "--command" "cabal\\ run")
+                 (progn
+                   (custom-set-variables '(haskell-process-wrapper-function (lambda (argv)
+                                                                              (append '("nix-shell" "default.nix" "--command")
+                                                                                      (list (shell-quote-argument argv))))))
+                   (apply haskell-process-wrapper-function (list "cabal run"))))))
 
 (ert-deftest test-haskell-process--compute-process-log-and-command-ghci ()
   (should (equal '("Starting inferior GHCi process ghci ..." "dumses1" nil "ghci" "-ferror-spans")
                  (let ((haskell-process-path-ghci "ghci")
-                       (haskell-process-args-ghci '("-ferror-spans"))
-                       (haskell-process-wrapper   nil))
+                       (haskell-process-args-ghci '("-ferror-spans")))
+                   (custom-set-variables '(haskell-process-wrapper-function #'identity))
                    (mocklet (((haskell-session-name "dummy-session") => "dumses1"))
                      (haskell-process-compute-process-log-and-command "dummy-session" 'ghci))))))
 
 (ert-deftest test-haskell-process--with-wrapper-compute-process-log-and-command-ghci ()
-  (should (equal '("Starting inferior GHCi process ghci ..." "dumses1" nil "nix-shell" "default.nix" "--command" "ghci -ferror-spans")
+  (should (equal '("Starting inferior GHCi process ghci ..." "dumses1" nil "nix-shell" "default.nix" "--command" "ghci\\ -ferror-spans")
                  (let ((haskell-process-path-ghci "ghci")
-                       (haskell-process-args-ghci '("-ferror-spans"))
-                       (haskell-process-wrapper   '("nix-shell" "default.nix" "--command")))
+                       (haskell-process-args-ghci '("-ferror-spans")))
+                   (custom-set-variables '(haskell-process-wrapper-function
+                                           (lambda (argv) (append (list "nix-shell" "default.nix" "--command" )
+                                                             (list (shell-quote-argument (mapconcat 'identity argv " ")))))))
                    (mocklet (((haskell-session-name "dummy-session") => "dumses1"))
                      (haskell-process-compute-process-log-and-command "dummy-session" 'ghci))))))
 
 (ert-deftest test-haskell-process--compute-process-log-and-command-cabal-repl ()
   (should (equal '("Starting inferior `cabal repl' process using cabal ..." "dumses2" nil "cabal" "repl" "--ghc-option=-ferror-spans" "dumdum-session")
                  (let ((haskell-process-path-cabal      "cabal")
-                       (haskell-process-args-cabal-repl '("--ghc-option=-ferror-spans"))
-                       (haskell-process-wrapper         nil))
+                       (haskell-process-args-cabal-repl '("--ghc-option=-ferror-spans")))
+                   (custom-set-variables '(haskell-process-wrapper-function #'identity))
                    (mocklet (((haskell-session-name "dummy-session2") => "dumses2")
                              ((haskell-session-target "dummy-session2") => "dumdum-session"))
                      (haskell-process-compute-process-log-and-command "dummy-session2" 'cabal-repl))))))
 
 (ert-deftest test-haskell-process--with-wrapper-compute-process-log-and-command-cabal-repl ()
-  (should (equal '("Starting inferior `cabal repl' process using cabal ..." "dumses2" nil "nix-shell" "default.nix" "--command" "cabal repl --ghc-option=-ferror-spans" "dumdum-session")
+  (should (equal '("Starting inferior `cabal repl' process using cabal ..." "dumses2" nil "nix-shell" "default.nix" "--command" "cabal\\ repl\\ --ghc-option\\=-ferror-spans" "dumdum-session")
                  (let ((haskell-process-path-cabal      "cabal")
-                       (haskell-process-args-cabal-repl '("--ghc-option=-ferror-spans"))
-                       (haskell-process-wrapper         '("nix-shell" "default.nix" "--command")))
+                       (haskell-process-args-cabal-repl '("--ghc-option=-ferror-spans")))
+                   (custom-set-variables '(haskell-process-wrapper-function
+                                           (lambda (argv) (append (list "nix-shell" "default.nix" "--command" )
+                                                             (list (shell-quote-argument (mapconcat 'identity argv " ")))))))
                    (mocklet (((haskell-session-name "dummy-session2") => "dumses2")
                              ((haskell-session-target "dummy-session2") => "dumdum-session"))
                      (haskell-process-compute-process-log-and-command "dummy-session2" 'cabal-repl))))))
 
 (ert-deftest test-haskell-process--compute-process-log-and-command-cabal-ghci ()
   (should (equal '("Starting inferior cabal-ghci process using cabal-ghci ..." "dumses3" nil "cabal-ghci")
-                 (let ((haskell-process-path-ghci "ghci")
-                       (haskell-process-wrapper   nil))
+                 (let ((haskell-process-path-ghci "ghci"))
+                   (custom-set-variables '(haskell-process-wrapper-function #'identity))
                    (mocklet (((haskell-session-name "dummy-session3") => "dumses3"))
                      (haskell-process-compute-process-log-and-command "dummy-session3" 'cabal-ghci))))))
 
 (ert-deftest test-haskell-process--with-wrapper-compute-process-log-and-command-cabal-ghci ()
   (should (equal '("Starting inferior cabal-ghci process using cabal-ghci ..." "dumses3" nil "nix-shell" "default.nix" "--command" "cabal-ghci")
-                 (let ((haskell-process-path-ghci "ghci")
-                       (haskell-process-wrapper   '("nix-shell" "default.nix" "--command")))
+                 (let ((haskell-process-path-ghci "ghci"))
+                   (custom-set-variables '(haskell-process-wrapper-function
+                                           (lambda (argv) (append (list "nix-shell" "default.nix" "--command" )
+                                                             (list (shell-quote-argument (mapconcat 'identity argv " ")))))))
                    (mocklet (((haskell-session-name "dummy-session3") => "dumses3"))
                      (haskell-process-compute-process-log-and-command "dummy-session3" 'cabal-ghci))))))
 
 (ert-deftest test-haskell-process--compute-process-log-and-command-cabal-dev ()
   (should (equal '("Starting inferior cabal-dev process cabal-dev -s directory/cabal-dev ..." "dumses4" nil "cabal-dev" "ghci" "-s" "directory/cabal-dev")
-                 (let ((haskell-process-path-cabal-dev "cabal-dev")
-                       (haskell-process-wrapper        nil))
+                 (let ((haskell-process-path-cabal-dev "cabal-dev"))
+                   (custom-set-variables '(haskell-process-wrapper-function #'identity))
                    (mocklet (((haskell-session-name "dummy-session4")      => "dumses4")
                              ((haskell-session-cabal-dir "dummy-session4") => "directory"))
                      (haskell-process-compute-process-log-and-command "dummy-session4" 'cabal-dev))))))
 
 (ert-deftest test-haskell-process--with-wrapper-compute-process-log-and-command-cabal-dev ()
-  (should (equal '("Starting inferior cabal-dev process cabal-dev -s directory/cabal-dev ..." "dumses4" nil "run-with-docker" "cabal-dev ghci -s directory/cabal-dev")
-                 (let ((haskell-process-path-cabal-dev "cabal-dev")
-                       (haskell-process-wrapper        "run-with-docker"))
+  (should (equal '("Starting inferior cabal-dev process cabal-dev -s directory/cabal-dev ..." "dumses4" nil "run-with-docker" "cabal-dev\\ ghci\\ -s\\ directory/cabal-dev")
+                 (let ((haskell-process-path-cabal-dev "cabal-dev"))
+                   (custom-set-variables '(haskell-process-wrapper-function
+                                           (lambda (argv) (append (list "run-with-docker")
+                                                             (list (shell-quote-argument (mapconcat 'identity argv " ")))))))
                    (mocklet (((haskell-session-name "dummy-session4") => "dumses4")
                              ((haskell-session-cabal-dir "dummy-session4") => "directory"))
                      (haskell-process-compute-process-log-and-command "dummy-session4" 'cabal-dev))))))
