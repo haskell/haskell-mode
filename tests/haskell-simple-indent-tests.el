aaa3fd7249d6f36b421bbefd90397ed3420ff5bd
Gracjan Polak <gracjan@scrive.com>
2015-01-10 14:55:02 +0100

    Add tests for haskell-simple-indent-backtab.
    Function haskell-simple-indent-backtab is supposed to work as a dual to
    haskell-simple-indent, this is not the case right now.
    
    Add two expected failure test cases that build up for upcoming fix.

--- tests/haskell-simple-indent-tests.el
+++ tests/haskell-simple-indent-tests.el
@@ -13,6 +13,23 @@
 	(setq result (cons (current-column) result)))
       (reverse result))))
 
+
+(defun find-indent-and-backtab-positions (lines-above-content)
+  (with-temp-buffer
+    (dolist (line lines-above-content)
+      (insert line)
+      (insert "\n"))
+    ;; cursor is at the beginning of the second line now
+    (let ((result-forward '())
+	  (result-backward '()))
+      (dotimes (i 10)
+	(haskell-simple-indent)
+	(setq result-forward (cons (current-column) result-forward)))
+      (dotimes (i 10)
+	(setq result-backward (cons (current-column) result-backward))
+	(haskell-simple-indent-backtab))
+      (list (reverse result-forward) result-backward))))
+
 (ert-deftest find-indent-positions-1 ()
   (should (equal '(5 7 10 19 26 32 40 48 56 64)
 		 (find-indent-positions '("main = do putStrLn \"Hello World!\"")))))
@@ -33,3 +50,16 @@
   (should (equal '(2 4 5 8 16 24 32 40 48 56)
 		 (find-indent-positions '("a b c d e f g h"
 					  "     long_streak")))))
+
+(ert-deftest find-indent-and-backtab-positions-1 ()
+  :expected-result :failed
+  (should (equal '((2 4 5 8 16 24 32 40 48 56)
+		   (2 4 5 8 16 24 32 40 48 56))
+		 (find-indent-and-backtab-positions '("a b c d e f g h"
+						      "     long_streak")))))
+
+(ert-deftest find-indent-and-backtab-positions-2 ()
+  :expected-result :failed
+  (should (equal '((8 10 13 20 24 27 32 35 37 45)
+		   (8 10 13 20 24 27 32 35 37 45))
+		 (find-indent-and-backtab-positions '("\tx <- return 123 {- This is a comment -}")))))
