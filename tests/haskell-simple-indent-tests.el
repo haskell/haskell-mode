bf9df35b69ca6f70f0d2d8dda4c8d1bea848a14e
Gracjan Polak <gracjan@scrive.com>
2015-01-16 20:02:41 +0100

    Tests for first column handling.

--- tests/haskell-simple-indent-tests.el
+++ tests/haskell-simple-indent-tests.el
@@ -22,14 +22,15 @@
       (insert line)
       (insert "\n"))
     ;; cursor is at the beginning of the second line now
-    (let ((result-forward '())
+    (let ((result-forward '(0))
 	  (result-backward '()))
-      (dotimes (i 10)
+      (dotimes (i 9)
 	(haskell-simple-indent)
 	(setq result-forward (cons (current-column) result-forward)))
-      (dotimes (i 10)
+      (dotimes (i 9)
 	(setq result-backward (cons (current-column) result-backward))
 	(haskell-simple-indent-backtab))
+      (setq result-backward (cons (current-column) result-backward))
       (list (reverse result-forward) result-backward))))
 
 (ert-deftest find-indent-positions-1 ()
@@ -64,8 +65,8 @@
                                           "")))))
 
 (ert-deftest find-indent-and-backtab-positions-1 ()
-  (should (equal '((2 4 5 8 16 24 32 40 48 56)
-		   (2 4 5 8 16 24 32 40 48 56))
+  (should (equal '((0 2 4 5 8 16 24 32 40 48)
+		   (0 2 4 5 8 16 24 32 40 48))
                  ;; Note: haskell-simple-indent-backtab is broken when
                  ;; it encounters TABs in source file.
 		 (find-indent-and-backtab-positions '("a b c d e f g h"
@@ -74,26 +75,26 @@
                                                       (setq indent-tabs-mode nil))))))
 
 (ert-deftest find-indent-and-backtab-positions-1a ()
-  (should (equal '((2 4 5 8 16 24 32 40 48 56)
-		   (2 4 5 8 16 24 32 40 48 56))
+  (should (equal '((0 2 4 5 8 16 24 32 40 48)
+		   (0 2 4 5 8 16 24 32 40 48))
 		 (find-indent-and-backtab-positions '("a b c d e f g h"
 						      "     long_streak")))))
 
 (ert-deftest find-indent-and-backtab-positions-2 ()
-  (should (equal '((8 10 13 20 24 27 32 35 37 45)
-		   (8 10 13 20 24 27 32 35 37 45))
+  (should (equal '((0 8 10 13 20 24 27 32 35 37)
+		   (0 8 10 13 20 24 27 32 35 37))
 		 (find-indent-and-backtab-positions '("\tx <- return 123 {- This is a comment -}")
                                                     (lambda ()
                                                       (setq-local indent-tabs-mode nil))))))
 
 (ert-deftest find-indent-and-backtab-positions-2a ()
-  (should (equal '((8 10 13 20 24 27 32 35 37 45)
-		   (8 10 13 20 24 27 32 35 37 45))
+  (should (equal '((0 8 10 13 20 24 27 32 35 37)
+		   (0 8 10 13 20 24 27 32 35 37))
 		 (find-indent-and-backtab-positions '("\tx <- return 123 {- This is a comment -}")))))
 
 (ert-deftest find-indent-and-backtab-positions-3 ()
-  (should (equal '((2 4 6 13 15 17 19 24 32 40)
-                   (2 4 6 13 15 17 19 24 32 40))
+  (should (equal '((0 2 4 6 13 15 17 19 24 32)
+                   (0 2 4 6 13 15 17 19 24 32))
 		 (find-indent-and-backtab-positions '(" f g e  e iirelevant"
                                                       "a b c d"
                                                       "             h idden     x"
@@ -105,8 +106,8 @@
                                                       (setq-local indent-tabs-mode nil))))))
 
 (ert-deftest find-indent-and-backtab-positions-3a ()
-  (should (equal '((2 4 6 13 15 17 19 24 32 40)
-                   (2 4 6 13 15 17 19 24 32 40))
+  (should (equal '((0 2 4 6 13 15 17 19 24 32)
+                   (0 2 4 6 13 15 17 19 24 32))
 		 (find-indent-and-backtab-positions '(" f g e  e iirelevant"
                                                       "a b c d"
                                                       "             h idden     x"
