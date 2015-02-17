9f1560ba5203362363954543f96816695bf5f160
Gracjan Polak <gracjan@scrive.com>
2015-02-18 00:41:35 +0100

    Use string-prefix-p instead of haskell-is-prefix-of.

--- haskell-string.el
+++ haskell-string.el
@@ -13,11 +13,6 @@
              0
              (min (length string) n)))
 
-;;;###autoload
-(defun haskell-is-prefix-of (x y)
-  "Is x string a prefix of y string?"
-  (string= x (substring y 0 (min (length y) (length x)))))
-
 (defun haskell-string ())
 
 (provide 'haskell-string)
