a6c732ad7d7d6a3b0838715058206d3f5061a3af
Tony Day <tonyday567@gmail.com>
2015-03-15 07:17:49 +1000

    included query element in url, and hexified query

--- haskell-mode.el
+++ haskell-mode.el
@@ -797,11 +797,14 @@ If nil, use the Hoogle web-site."
   :type '(choice (const :tag "Use Web-site" nil)
                  string))
 
-(defcustom haskell-hoogle-url "http://haskell.org/hoogle/"
+(defcustom haskell-hoogle-url "http://haskell.org/hoogle/?q=%s"
   "Default value for hoogle web site.
 "
   :group 'haskell
-  :type '(choice (const :tag "haskell-org" "http://haskell.org/hoogle/") (const :tag "fp-complete" "https://www.fpcomplete.com/hoogle") string))
+  :type '(choice
+          (const :tag "haskell-org" "http://haskell.org/hoogle/?q=%s")
+          (const :tag "fp-complete" "https://www.fpcomplete.com/hoogle?q=%s")
+          string))
 
 ;;;###autoload
 (defun haskell-hoogle (query &optional info)
@@ -820,7 +823,7 @@ is asked to show extra info for the items matching QUERY.."
                         nil nil def)
            current-prefix-arg)))
   (if (null haskell-hoogle-command)
-      (browse-url (format "%s?q=%s" haskell-hoogle-url query))
+      (browse-url (format haskell-hoogle-url (url-hexify-string query)))
     (let ((hoogle-args (append (when info '("-i"))
                                (list "--color" (shell-quote-argument query)))))
       (with-help-window "*hoogle*"
