875a79a2fac0510496ee9ec8a101e197e2761c12
Gracjan Polak <gracjanpolak@gmail.com>
2015-09-02 16:49:24 +0200

    Add anim showing import statement completion

--- doc/gifcasts/gifcasts.el
+++ doc/gifcasts/gifcasts.el
@@ -77,3 +77,54 @@
 
        (kill-buffer "Main.hs")))))))
  nil)
+
+(gifcast-animate
+ (set-frame-size (window-frame (get-buffer-window)) 70 15)
+ (when (get-buffer "Main.hs")
+   (kill-buffer "Main.hs"))
+ (switch-to-buffer (get-buffer-create "Main.hs"))
+ (set-visited-file-name "Main.hs" t)
+ (delete-other-windows)
+ (tabbar-mode -1)
+ (tool-bar-mode -1)
+ (linum-mode -1)
+ (blink-cursor-mode -1)
+ (message nil)
+
+ (haskell-mode)
+ (company-mode)
+ (setq company-idle-delay 0.01)
+ (linum-mode -1)
+
+ (insert (concat
+          "\n\n\n"
+          "main :: IO ()\n"
+          "main = return ()\n"))
+ (save-buffer)
+
+ (haskell-process-load-file)
+ (sit-for 5)
+ (select-window (get-buffer-window "Main.hs"))
+
+ (goto-char (+ 1 (point-min)))
+ (gifcast-keys-async
+  ""
+  (gifcast-capture)
+  (gifcast-keys-async
+   "import"
+   (gifcast-capture)
+   (gifcast-keys-async
+    " Control."
+    (gifcast-capture)
+    (gifcast-keys-async
+     "A"
+     (gifcast-capture)
+     (gifcast-keys-async
+      "\C-m"
+      (gifcast-capture)
+      (gifcast-generate "../anim/company-mode-import-statement.gif")
+
+      (haskell-kill-session-process)
+      (set-buffer-modified-p nil)
+      (kill-buffer "Main.hs"))))))
+ nil)
