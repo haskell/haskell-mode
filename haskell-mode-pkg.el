94ad07d5095257bf06e1201f0c817f5be9a16c60
tsukimizake <shomasd@gmail.com>
2015-10-16 20:53:21 +0900

    Add ido/helm interface for history

--- haskell-mode-pkg.el
+++ haskell-mode-pkg.el
@@ -1,5 +1,6 @@
 (define-package "haskell-mode" "20150822.1728" "A Haskell editing mode"
-		'((cl-lib "0.5")))
+		'((cl-lib "0.5")
+		  (helm "20151013.958")))
 ;; Local Variables:
 ;; no-byte-compile: t
 ;; End:
