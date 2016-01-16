6f828c13411e2aab797389c796c0bc0f17425716
Arthur Fayzrakhmanov (Артур Файзрахманов) <heraldhoi@gmail.com>
2016-01-17 00:03:40 +0500

    Add proper package biolerplate wrap

--- tests/haskell-utils-tests.el
+++ tests/haskell-utils-tests.el
@@ -1,3 +1,32 @@
+;;; haskell-utils-tests.el --- Tests for Haskell utilities package
+
+;; Copyright © 2016 Athur Fayzrakhmanov. All rights reserved.
+
+;; This file is part of haskell-mode package.
+;; You can contact with authors using GitHub issue tracker:
+;; https://github.com/haskell/haskell-mode/issues
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+;; Boston, MA 02110-1301, USA.
+
+;;; Commentary:
+
+;; This package provides regression tests for haskell-utils package.
+
+;;; Code:
+
 (require 'ert)
 (require 'haskell-utils)
 
@@ -100,3 +129,5 @@
                    (goto-char (point-min))
                    (forward-line 0)
                    (haskell-utils-parse-import-statement-at-point)))))
+
+;;; haskell-utils-tests.el ends here
