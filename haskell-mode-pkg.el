e37c4e53775067ecc0dad67976bb10971b2b118f
Gracjan Polak <gracjanpolak@gmail.com>
2016-01-22 20:27:25 +0100

    Update version info 13.18

--- haskell-mode-pkg.el
+++ haskell-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "haskell-mode" "13.17-git" "A Haskell editing mode"
+(define-package "haskell-mode" "13.18" "A Haskell editing mode"
   '((cl-lib "0.5"))
   :url "https://github.com/haskell/haskell-mode"
   :keywords '("haskell" "cabal" "ghc" "repl"))
