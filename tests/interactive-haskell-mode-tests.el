b9df6006dba36b4383d72ff13e6a661c5f2d2f47
Chris Done <chrisdone@gmail.com>
2016-02-19 10:31:05 +0000

    Revert "Remove suggested imports completely"

--- tests/interactive-haskell-mode-tests.el
+++ tests/interactive-haskell-mode-tests.el
@@ -32,21 +32,20 @@
 (require 'ert)
 (require 'haskell-interactive-mode)
 
+(defun should-match (str)
+  (should (eq 0 (string-match-p haskell-interactive-mode-error-regexp str))))
+
 (ert-deftest haskell-interactive-error-regexp-test ()
   "Tests the regexp `haskell-interactive-mode-error-regexp'"
   (should (eq 0 (string-match-p haskell-interactive-mode-error-regexp
                                 "/home/user/Test.hs:24:30:")))
   (should (eq 0 (string-match-p haskell-interactive-mode-error-regexp
                                 "Test.hs:5:18:")))
-  (should (eq 0 (string-match-p
-                 haskell-interactive-mode-error-regexp
-                 "Test.hs:7:6: Not in scope: type constructor or class ‘Ty’")))
+  (should (eq 0 (string-match-p haskell-interactive-mode-error-regexp
+                                "Test.hs:7:6: Not in scope: type constructor or class ‘Ty’")))
   (should (eq 0 (string-match-p haskell-interactive-mode-error-regexp
                                 "Test.hs:9:5: Not in scope: ‘c’")))
   (should (eq nil (string-match-p haskell-interactive-mode-error-regexp
                                   ;; leading space
                                   " Test.hs:8:9:")))
   )
-
-(provide 'interactive-haskell-mode-tests)
-;;; interactive-haskell-mode-tests.el ends here
