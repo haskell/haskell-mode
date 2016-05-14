225584f660cca3bd3d09b2944e6b285acbfc0ad5
Gracjan Polak <gracjanpolak@gmail.com>
2016-05-14 18:11:12 +0200

    Refactoring for more declarative feel

--- tests/haskell-code-conventions.el
+++ tests/haskell-code-conventions.el
@@ -8,7 +8,38 @@
     (package-install 'el-search))
   (require 'el-search))
 
-(defun haskell-check-conventions-in-current-buffer ()
+(defconst haskell-code-conventions
+  '((require-cl
+     error `(require 'cl)
+     "Do not use (require 'cl) use (require 'cl-lib) instead")
+    (shell-command
+     warning (or `(shell-command . ,_) `(shell-command-to-string . ,_))
+     "Do not use (shell-command[-to-string] ...) use (call-process ...) or (start-process ...) instead")
+    (message-non-const
+     warning (and `(message ,msg . ,_) (guard (not (stringp `,msg))))
+     "First argument to (message ...) should be a string literal")
+    (add-to-list
+     warning (and `(add-to-list (quote ,var) . ,_)
+                  (guard (not (member `,var '(completion-ignored-extensions
+                                              auto-mode-alist
+                                              interpreter-mode-alist
+                                              minor-mode-overriding-map-alist
+                                              haskell-sessions
+                                              flymake-allowed-file-name-masks
+                                              haskell-cabal-buffers)))))
+     "Use add-to-list only with dynamically scoped variables, whitelist is in `haskell-code-conventions.el'")
+    (when-not
+     warning `(when (not . ,_) . ,_)
+     "Use (unless ...) instead of (when (not ...))")
+    (unless-not
+     warning `(unless (not . ,_) . ,_)
+     "Use (when ...) instead of (unless (not ...))")
+    (nested-when-or-unless
+     warning ;;`(,(or `unless `when) `(,(or `unless `when) . ,_))
+     `(,(or `when `unless) ,_ (,(or `when `unless) . ,_))
+     "Nested when/unless, consider using logical operators instead of nesting")))
+
+(defun haskell-check-char-conventions-in-current-buffer ()
   "Check elisp convention suitable for haskell-mode in current buffer."
   (interactive)
 
@@ -17,18 +48,22 @@
       ;; check lexical binding
       (goto-char (point-min))
       (unless (re-search-forward "lexical-binding: t" (line-end-position) t)
-        (unless (member (file-name-nondirectory (buffer-file-name))
-                        '("haskell-wy.el" "haskell-mode-pkg.el" "haskell-mode-autoloads.el"))
-          (message "%s:1:0: Error: Use `lexical-binding: t` on the first line of file"
-                   (buffer-file-name))
-          (setq fail-flag t)))
+
+        (message "%s:1:0: Error: Use `lexical-binding: t` on the first line of file"
+                 (file-relative-name (buffer-file-name)
+                                     (with-current-buffer "*scratch*"
+                                       default-directory)))
+        (setq fail-flag t))
+
       ;; check TABS
       (goto-char (point-min))
       (while (re-search-forward "\t" nil t)
         (save-excursion
           (goto-char (match-beginning 0))
           (message "%s:%d:%d: Error: Do not use literal TAB characters"
-                   (buffer-file-name) (line-number-at-pos)
+                   (file-relative-name (buffer-file-name)
+                                       (with-current-buffer "*scratch*"
+                                         default-directory)) (line-number-at-pos)
                    (current-column))
           (setq fail-flag t)))
       ;; check spaces or tabs at the end of line
@@ -37,67 +72,52 @@
         (save-excursion
           (goto-char (match-beginning 0))
           (message "%s:%d:%d: Error: Do not use TABs or spaces at the end of line"
-                   (buffer-file-name) (line-number-at-pos)
+                   (file-relative-name (buffer-file-name)
+                                       (with-current-buffer "*scratch*"
+                                         default-directory)) (line-number-at-pos)
                    (current-column)))
-          (setq fail-flag t))
-      ;; check constructs we do not like
-      (when (featurep 'el-search)
+        (setq fail-flag t)))
 
-        (goto-char (point-min))
-        (while (el-search--search-pattern `'(require 'cl) t)
-          (unless (save-excursion
-                    (goto-char (line-beginning-position))
-                    (re-search-forward "el-search--search-pattern" (line-end-position) t))
-            (message "%s:%d:%d: Error: Do not use (require 'cl) use (require 'cl-lib) instead"
-                     (buffer-file-name) (line-number-at-pos)
-                     (current-column))
-            (setq fail-flag t))
-          (el-search--skip-expression nil t))
-
-        ;; check against shell-command
-        (goto-char (point-min))
-        (while (el-search--search-pattern '(or `(shell-command . ,_) `(shell-command-to-string . ,_)) t)
-          (unless (save-excursion
-                    (goto-char (line-beginning-position))
-                    (re-search-forward "el-search--search-pattern" (line-end-position) t))
-            ;; for now this is a warning
-            (message "%s:%d:%d: Warning: Do not use (shell-command[-to-string] ...) use (call-process ...) or (start-process ...) instead"
-                     (buffer-file-name) (line-number-at-pos)
-                     (current-column)))
-          (el-search--skip-expression nil t))
-
-        ;; check against message with non-constant first argument
-        (goto-char (point-min))
-        (while (el-search--search-pattern '(and `(message ,msg . ,_) (guard (not (stringp `,msg)))) t)
-          (unless (save-excursion
-                    (goto-char (line-beginning-position))
-                    (re-search-forward "el-search--search-pattern" (line-end-position) t))
-            ;; for now this is a warning
-            (message "%s:%d:%d: Warning: First argument to (message ...) must be constant string"
-                     (buffer-file-name) (line-number-at-pos)
-                     (current-column)))
-          (el-search--skip-expression nil t))
-
-        ;; check against non-toplevel add-to-list that has issues working with lexical variables
-        (goto-char (point-min))
-        (while (el-search--search-pattern '(and `(add-to-list (quote ,var) . ,_)
-                                                (guard (not (member `,var '(completion-ignored-extensions
-                                                                            auto-mode-alist
-                                                                            interpreter-mode-alist
-                                                                            minor-mode-overriding-map-alist
-                                                                            haskell-sessions
-                                                                            flymake-allowed-file-name-masks
-                                                                            haskell-cabal-buffers))))) t)
-          (unless (save-excursion
-                    (goto-char (line-beginning-position))
-                    (re-search-forward "el-search--search-pattern" (line-end-position) t))
-            (message "%s" (buffer-substring-no-properties (line-beginning-position) (line-end-position)))
-            (message "%s:%d:%d: Error: add-to-list is only allowed on whitelisted variables because it does not work with lexical-binding, use cl-pushnew with :test #'equal"
-                     (buffer-file-name) (line-number-at-pos)
-                     (current-column))
-            (setq fail-flag t))
-          (el-search--skip-expression nil t))))
     fail-flag))
+
+(defun haskell-check-sexp-conventions-in-current-buffer ()
+  "Check elisp convention suitable for haskell-mode in current buffer."
+  (interactive)
+
+  (when (featurep 'el-search)
+    (let (fail-flag)
+      (save-excursion
+        (dolist (convention haskell-code-conventions)
+          (unless (equal 'disabled (nth 1 convention))
+            (goto-char (point-min))
+            (while (el-search--search-pattern `(and ,(nth 2 convention) (guard (not (save-excursion
+                                                                                      (beginning-of-defun)
+                                                                                      (looking-at-p "(defconst haskell-code-conventions"))))) t)
+              (message "%s:%d:%d: %s: %s"
+                       (file-relative-name (buffer-file-name)
+                                           (with-current-buffer "*scratch*"
+                                             default-directory)) (line-number-at-pos)
+                                             (current-column)
+                                             (if (equal 'error (nth 1 convention)) "Error" "Warning")
+                                             (nth 3 convention))
+              (message "%s"
+                       (replace-regexp-in-string
+                        "^" "  " (buffer-substring-no-properties
+                                  (line-beginning-position)
+                                  (save-excursion
+                                    (goto-char (el-search--end-of-sexp))
+                                    (line-end-position)))))
+              (when (equal 'error (nth 1 convention))
+                (setq fail-flag t))
+              (el-search--skip-expression nil t)))))
+
+      fail-flag)))
+
+(defun haskell-check-conventions-in-current-buffer ()
+  "Check elisp convention suitable for haskell-mode in current buffer."
+  (interactive)
+  (or (haskell-check-char-conventions-in-current-buffer)
+      (haskell-check-sexp-conventions-in-current-buffer)))
 
 (defun haskell-check-conventions ()
   "Check elisp coding conventions in elisp files comprising haskell-mode.
@@ -115,11 +135,15 @@ Return non-nil when conventions were violated in any of the files."
          fail-flag)
 
     (dolist (file files)
-      (with-current-buffer
-          (find-file-noselect file)
-        (when (haskell-check-conventions-in-current-buffer)
-          (setq fail-flag t))
-  (kill-buffer (current-buffer))))
+      (unless (member (file-name-nondirectory file)
+                      '("haskell-wy.el" "haskell-mode-pkg.el" "haskell-mode-autoloads.el"
+                        ;; old files we do not care about
+                        "haskell-indent.el" "haskell-decl-scan.el"))
+        (with-current-buffer
+            (find-file-noselect file)
+          (when (haskell-check-conventions-in-current-buffer)
+            (setq fail-flag t))
+          (kill-buffer (current-buffer)))))
     fail-flag))
 
 (defun haskell-check-conventions-batch-and-exit ()
