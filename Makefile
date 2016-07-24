9263b221b333950347567849e68c2576f74ae744
Gracjan Polak <gracjanpolak@gmail.com>
2016-07-24 18:28:06 +0200

    Update version info to 13.20.1

--- Makefile
+++ Makefile
@@ -10,7 +10,7 @@
 # We should have a script that changes it everywhere it is needed and
 # syncs it with current git tag.
 #
-VERSION = 13.20
+VERSION = 13.20.1
 
 INSTALL_INFO = install-info
 
--- haskell-mode-pkg.el
+++ haskell-mode-pkg.el
@@ -1,4 +1,4 @@
-(define-package "haskell-mode" "13.20" "A Haskell editing mode"
+(define-package "haskell-mode" "13.20.1" "A Haskell editing mode"
   '((emacs "24.1") (cl-lib "0.5"))
   :url "https://github.com/haskell/haskell-mode"
   :keywords '("haskell" "cabal" "ghc" "repl"))
