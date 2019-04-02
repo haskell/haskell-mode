3cea814ca1eb078340f75c42302fdf7119a2219b
Hussein Ait-Lahcen <hussein.ait-lahcen@fretlink.com>
2019-04-02 11:58:06 +0200

    introduce `anyclass`, `stock` and `via` keywords

--- haskell-font-lock.el
+++ haskell-font-lock.el
@@ -93,7 +93,8 @@ be disabled at that position."
   '("case" "class" "data" "default" "deriving" "do"
     "else" "if" "import" "in" "infix" "infixl"
     "infixr" "instance" "let" "module" "mdo" "newtype" "of"
-    "rec" "pattern" "proc" "signature" "then" "type" "where" "_")
+    "rec" "pattern" "proc" "signature" "then" "type" "where" "_"
+    "anyclass" "stock" "via")
   "Identifiers treated as reserved keywords in Haskell."
   :group 'haskell-appearance
   :type '(repeat string))
