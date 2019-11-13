0cbb25e7cfac7c27a6e359b8db2770f4501b75a3
Steve Purcell <steve@sanityinc.com>
2019-11-14 11:25:14 +1300

    Some code requires Emacs 25.1, so make that explicit

--- haskell-mode-pkg.el
+++ haskell-mode-pkg.el
@@ -1,5 +1,5 @@
 (define-package "haskell-mode" "16.2-git" "A Haskell editing mode"
-  '((emacs "24.4"))
+  '((emacs "25.1"))
   :url "https://github.com/haskell/haskell-mode"
   :keywords '("haskell" "cabal" "ghc" "repl"))
 ;; Local Variables:
