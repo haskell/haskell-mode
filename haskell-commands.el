0d5e4136668166c0edb259bb9accf09d257dcf68
Steve Purcell <steve@sanityinc.com>
2019-12-19 14:19:18 +1300

    Force UTF-8 when running code through stylish-haskell
    See #1654

--- haskell-commands.el
+++ haskell-commands.el
@@ -814,39 +814,41 @@ Use buffer as input and replace the whole buffer with the
 output.  If CMD fails the buffer remains unchanged."
   (set-buffer-modified-p t)
   (let* ((out-file (make-temp-file "stylish-output"))
-         (err-file (make-temp-file "stylish-error")))
-        (unwind-protect
-          (let* ((_errcode
-                  (apply 'call-process-region (point-min) (point-max) cmd nil
-                         `((:file ,out-file) ,err-file)
-                         nil args))
-                 (err-file-empty-p
-                  (equal 0 (nth 7 (file-attributes err-file))))
-                 (out-file-empty-p
-                  (equal 0 (nth 7 (file-attributes out-file)))))
-            (if err-file-empty-p
-                (if out-file-empty-p
-                    (message "Error: %s produced no output and no error information, leaving buffer alone" cmd)
-                  ;; Command successful, insert file with replacement to preserve
-                  ;; markers.
-                  (insert-file-contents out-file nil nil nil t))
-              (progn
-                ;; non-null stderr, command must have failed
-                (with-current-buffer
-                    (get-buffer-create "*haskell-mode*")
-                  (insert-file-contents err-file)
-                  (buffer-string))
-                (message "Error: %s ended with errors, leaving buffer alone, see *haskell-mode* buffer for stderr" cmd)
-                (with-temp-buffer
-                  (insert-file-contents err-file)
-                  ;; use (warning-minimum-level :debug) to see this
-                  (display-warning cmd
-                                   (buffer-substring-no-properties (point-min) (point-max))
-                                   :debug)))))
-          (ignore-errors
-            (delete-file err-file))
-          (ignore-errors
-            (delete-file out-file)))))
+         (err-file (make-temp-file "stylish-error"))
+         (coding-system-for-read 'utf-8)
+         (coding-system-for-write 'utf-8))
+    (unwind-protect
+        (let* ((_errcode
+                (apply 'call-process-region (point-min) (point-max) cmd nil
+                       `((:file ,out-file) ,err-file)
+                       nil args))
+               (err-file-empty-p
+                (equal 0 (nth 7 (file-attributes err-file))))
+               (out-file-empty-p
+                (equal 0 (nth 7 (file-attributes out-file)))))
+          (if err-file-empty-p
+              (if out-file-empty-p
+                  (message "Error: %s produced no output and no error information, leaving buffer alone" cmd)
+                ;; Command successful, insert file with replacement to preserve
+                ;; markers.
+                (insert-file-contents out-file nil nil nil t))
+            (progn
+              ;; non-null stderr, command must have failed
+              (with-current-buffer
+                  (get-buffer-create "*haskell-mode*")
+                (insert-file-contents err-file)
+                (buffer-string))
+              (message "Error: %s ended with errors, leaving buffer alone, see *haskell-mode* buffer for stderr" cmd)
+              (with-temp-buffer
+                (insert-file-contents err-file)
+                ;; use (warning-minimum-level :debug) to see this
+                (display-warning cmd
+                                 (buffer-substring-no-properties (point-min) (point-max))
+                                 :debug)))))
+      (ignore-errors
+        (delete-file err-file))
+      (ignore-errors
+        (delete-file out-file)))))
 
 ;;;###autoload
 (defun haskell-mode-find-uses ()
