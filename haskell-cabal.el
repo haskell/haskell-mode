f95b818662b625af6d7210d1451e1bb1fcbb455b
Frank Staals <frank@fstaals.net>
2023-02-02 21:37:34 +0100

    fixed stack syntax

--- haskell-cabal.el
+++ haskell-cabal.el
@@ -515,7 +515,9 @@ PROCESS-TYPE determines the format of the returned target."
                        (val (car (split-string
                                   (haskell-cabal-section-value section)))))
                   (if (equal (downcase component-type) "library")
-                      (let* ((lib-name (if (eq 'val "") package-name val))
+                      (let* ((lib-name (if (not val)
+                                           (if (eq 'stack-ghci process-type) "lib" package-name)
+                                           val))
                              (lib-target (concat package-name ":" lib-name))
                              )
                         (push lib-target matches))
